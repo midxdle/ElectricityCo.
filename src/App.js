643c2792b0027692f8018d9e9246312c29a4b3ab
midxdle <sina.heydarian.sh@gmail.com>
2022-02-23 19:28:29 +0330

    nothing important

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import Top from "./components/Top/Top";
 import Header from "./components/Header/Head";
 import Slider from "./components/Slider/Slider";
-import SectionWhy from "./components/SectionWhy/SectionWhy";
 import SecFive from "./components/SectionFive/SecFive";
-import SecSix from "./components/SectionSix/SecSix";
-import SecSeven from "./components/SectionSeven/SecSeven";
 import SecNews from "./components/SecNews/SecNews";
 import SecUse from "./components/SecUse/SecUse";
 import SectionSupport from "../src/components/SectionSupport/SectionSupport"
@@ -17,9 +14,6 @@ function App() {
       <Header />
       <Slider />
       <SecFive />
-      <SectionWhy />
-      <SecSix />
-      <SecSeven />
       <SecNews />
       <SecUse />
       <SectionSupport />
