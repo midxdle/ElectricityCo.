10f00e547d3f6f1df0737893f15545d00618e6f6
midxdle <sina.heydarian.sh@gmail.com>
2022-03-06 15:00:30 +0330

    just add login and sign up component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import SecFive from "./components/SectionFive/SecFive";
 import SecNews from "./components/SecNews/SecNews";
 import SecUse from "./components/SecUse/SecUse";
 import Footer from "./components/Footer/Footer";
+import LoginSignup from "./components/LoginSignup/LoginSignup";
 
 function App() {
   return (
